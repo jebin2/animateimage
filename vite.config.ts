e08fdf3e1b92176460cee7693b3d1dd252afdcca
Jebin Einstein <jebineinstein@gmail.com>
2025-12-17 20:05:10 +0530

    port set

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,14 @@ export default defineConfig(({ mode }) => {
   return {
     base: '/animateimage/',
     server: {
-      port: 3000,
+      port: 3001,
       host: '0.0.0.0',
+      strictPort: true
+    },
+    preview: {
+      port: 3001,
+      host: '0.0.0.0',
+      strictPort: true
     },
     plugins: [react()],
     define: {
